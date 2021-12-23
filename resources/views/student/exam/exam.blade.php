e8c5aefe6dc3801b0d69b1c5cfc8ee45c3a0c3c1
aruppanda01 <arup.panda@onenesstechs.in>
2021-12-23 10:57:08 +0530

    Exam design issue fixed

--- resources/views/student/exam/exam.blade.php
+++ resources/views/student/exam/exam.blade.php
@@ -13,22 +13,38 @@
                     </div>
                 </div>
             </div>
-            <h5>Remaining time:  <span id="remaining_time" class="badge badge-primary">{{ $exam_time }}</span></h5>
+            <div class="remain_type sticky">
+                <h5>Remaining time:  <span id="remaining_time" class="badge badge-primary">{{ $exam_time }}</span></h5>
+            </div>
             <div class="tabs-animation">
                 {{-- It restrict user from copy,paste and cut --}}
                 {{-- <section class=" " oncopy="return false;" oncut="return false;" onpaste="return false;"
                     oncontextmenu="return false;"> --}}
                     <div class="card mb-3">
-                        <div class="card-header justify-content-center">
+                        <div class="card-header qtitle justify-content-center">
                             Leading Lights
                         </div>
-                        <div>
-                            <h5 class="card-tittle text-center">Class: {{ Auth::user()->class_details->name}}</h5>
-                            <h5 class="card-tittle text-center">Subject: {{ $exam->subject_details->name}}</h5>
-                            <h5 class="card-tittle text-center">Exam type: {{ $exam->type_of_exam}}</h5>
+                        <div class="row m-0 exam_header align-items-center">
+                            <div class="col-12 col-lg-4">
+                                <ul class="mb-0">
+                                    <li>Date : <b>{{ date('d-M-Y',strtotime($exam->date)) }}</b> </li>
+                                    <li>Student ID : <b>{{ Auth::user()->id_no }}</b></li>
+                                    <li>Student Name : <b>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</b></li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <h5 class="card-tittle text-center">Class : <b>{{ Auth::user()->class_details->name}}</b></h5>
+                                <h5 class="card-tittle text-center">Subject : <b>{{ $exam->subject_details->name}}</b></h5>
+                                <h5 class="card-tittle text-center">Name of exam : <b>{{ $exam->type_of_exam}}</b></h5>
+                            </div>
+                            <div class="col-12 col-lg-4 text-right">
+                                <ul class="mb-0">
+                                <li>Total Marks : <b>{{ $exam->full_marks }}</b></li>
+                                <li>Pass Marks : <b>{{ $exam->pass_marks }}</b></li>
+                            </ul>
+                            </div>
                         </div>
-                        <hr>
-                        <div>
+                        <!--<div>
                             <ul>
                                 <li>Date: {{ date('d-M-Y',strtotime($exam->date)) }} </li>
                                 <li>Student ID: {{ Auth::user()->id_no }}</li>
@@ -38,16 +54,16 @@
                                 <li>Total Marks: {{ $exam->full_marks }}</li>
                                 <li>Pass Marks: {{ $exam->pass_marks }}</li>
                             </ul>
-                        </div>
+                        </div>-->
                         <div class="card-body">
-                            <p class="text-danger"><span class="font-weight-bold">Note: </span> All questions are compulsory.
+                            <p class="text-danger"><small><b>Note:</b> All questions are compulsory.</span>
                             @if ($exam->exam_type == 1 && $exam->negative_marks == 1)
                                 <span>If wrong question selected then 0.25 marks deducted</span>
                             @endif
                             </p>
                             <hr>
                             @if ($questions->count() > 0)
-                                <form action="{{ route('user.exam.start', $exam->id) }}" method="POST" name="exam"
+                                <form action="{{ route('user.exam.start', $exam->id) }}" class="exam_form" method="POST" name="exam"
                                     id="examForm">
                                     @csrf
                                     @foreach ($questions as $key => $question)
@@ -85,7 +101,7 @@
                                             @php
                                                 $stored_answer = App\Models\TempExam::where('question_id',$question->id)->where('user_id',Auth::user()->id)->first();
                                             @endphp
-                                            <div class="col-md-10">
+                                            <div class="col-md-12">
                                                 <div class="form-group">
                                                     <label for="answer"><b>Answer: </b></label>
                                                     @if ($stored_answer)
@@ -96,38 +112,41 @@
 
                                                 </div>
                                             </div>
-                                            <div class="col-md-2">
-                                                <button type="submit" class="btn btn-primary" id="save_ans" value="save_ans" name="save_ans" onclick="saveAnswer({{ $exam->id }},{{ $question->id }},{{ $key + 1}})"><i class="fa fa-save"></i> Save</button>
+                                            <div class="col-md-12">
+                                                <button type="submit" class="btn save_btn" id="save_ans" value="save_ans" name="save_ans" onclick="saveAnswer({{ $exam->id }},{{ $question->id }},{{ $key + 1}})"><i class="fa fa-save"></i> Save</button>
                                             </div>
                                         </div>
                                         @endif
                                         @if ($exam->exam_type == 3 && $question->question_type == 2)
                                             <div class="row">
-                                                <div class="col-md-10">
+                                                <div class="col-md-12">
                                                     <div class="form-group">
                                                         <label for="answer"><b>Answer: </b></label>
                                                         <textarea name="answer{{ $key + 1 }}" id="" cols="3" rows="3" class="form-control" id="answer"></textarea>
                                                     </div>
                                                 </div>
-                                                <div class="col-md-2">
-                                                    <button type="submit"><i class="fa fa-save"></i> Save</button>
+                                                <div class="col-md-12">
+                                                    <button type="submit" class="btn save_btn"><i class="fa fa-save"></i> Save</button>
                                                 </div>
                                             </div>
 
                                         @endif
                                         <input type="hidden" name="question_type[]" value="{{ $question->question_type }}">
-                                        <p class="font-weight-bold">Marks: <span>{{ $question->marks }}</span></p>
+                                        <p class="marks_text">Marks: <span>{{ $question->marks }}</span></p>
                                         <hr>
                                     @endforeach
-                                    <input type="hidden" name="index" value="{{ $key + 1 }}">
-                                    <input type="hidden" name="exam_id" value="{{ $exam->id }}">
-                                    <input type="hidden" name="is_user_submitted" value="0" id="is_user_submitted">
+                                    <div class="text-right">
+                                        <input type="hidden" name="index" value="{{ $key + 1 }}">
+                                        <input type="hidden" name="exam_id" value="{{ $exam->id }}">
+                                        <input type="hidden" name="is_user_submitted" value="0" id="is_user_submitted">
 
-                                    <input type="submit" value="submit" class="btn btn-primary btn-lg"
-                                    id="btn_submit" name="btn_submit">
+                                        <input type="submit" value="submit" class="btn btn-primary submit_btn btn-lg"
+                                        id="btn_submit" name="btn_submit">
+                                    </div>
+
                                 </form>
                             @else
-                                <h2 class="text-danger">Oops. No data found</h2>
+                                <h5 class="text-danger">Oops. No data found</h5>
                             @endif
 
                         </div>
@@ -297,7 +316,7 @@
 
         // Confirmation before submit an exam
         $('#btn_submit').on('click',function() {
-            event.preventDefault(); 
+            event.preventDefault();
             const swalWithBootstrapButtons = Swal.mixin({
                 customClass: {
                     confirmButton: 'btn btn-success',
