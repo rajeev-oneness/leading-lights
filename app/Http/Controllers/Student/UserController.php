c83c8e539da356cf85f83eb871f4f384eb4fd53e
Arup Panda <arup.panda@onenesstechs.in>
2022-01-31 11:09:05 +0530

    Transaction little changes

--- app/Http/Controllers/Student/UserController.php
+++ app/Http/Controllers/Student/UserController.php
@@ -186,7 +186,7 @@ class UserController extends Controller
         $data = (object)[];
         $user = Auth::user();
         $data->due_payment = \App\Models\Fee::where('user_id', $user->id)->where('transaction_id', 0)->latest('id')->get();
-        $data->success_payment = \App\Models\Fee::where('user_id', $user->id)->where('transaction_id', '>', 0)->get();
+        $data->success_payment = \App\Models\Fee::where('user_id', $user->id)->where('transaction_id', '>', 0)->latest('id')->get();
         return view('student.payments', compact('data'));
     }
 
